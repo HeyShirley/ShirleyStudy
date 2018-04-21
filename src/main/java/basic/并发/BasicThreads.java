eeac61998b9bca7fc2b554b5cbda7dddac92e806
赵雪宇 <2785560785@qq.com>
2018-04-21 17:14:50 +0800

    update

--- src/main/java/basic/并发/BasicThreads.java
+++ src/main/java/basic/并发/BasicThreads.java
@@ -7,6 +7,7 @@ public class BasicThreads {
     public static void main(String[] args){
         Thread t = new Thread(new LiftOff());
         t.start();
+
         System.out.println("Waiting for LiftOff");
     }
 }
