0d3e80e9d2f4d96696bc65bb3ad0c4108d95a842
赵雪宇 <2785560785@qq.com>
2018-04-22 21:40:49 +0800

    AspectJ自动代理

--- src/main/java/Spring/concert/ConcertConfig.java
+++ src/main/java/Spring/concert/ConcertConfig.java
@@ -1,7 +1,20 @@
 package Spring.concert;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
 /**
+ * EnableAspectJAutoProxy注解，启用AspectJ自动代理功能
  * Created by zhaoxueyu on 2018/4/22.
  */
+@Configuration
+@EnableAspectJAutoProxy
+@ComponentScan
 public class ConcertConfig {
+    @Bean
+    public Audience audience(){
+        return new Audience();
+    }
 }
